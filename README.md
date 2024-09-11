fcf0196c959db3cd689865fe19afd8b52f55f562
chestnutchest <180890421+chestnutchest@users.noreply.github.com>
2024-09-11 00:51:21 -0700

    added license, README, and schema.sql

---
+++ README.md
@@ -0,0 +1,72 @@
+# Economics, Demographics, and Sporting Prowess
+### A SQL (PostgreSQL) Data Analytic Project
+
+A SQL data analytic project exploring the relationships between sporting strength and various economic and demographic metrics of countries and territories from across the world.
+
+Using PostgreSQL, I will be answering questions like:
+* Who has the highest GDP per capita but failed to win any medals at the Olympics?
+* Who has the higehst number of medals per million residents?
+* Which continent won the highest number of medals?
+* And many more!
+
+## Table of Contents
+
+[Quick Navigation](#quick-navigation)\
+[Project Description](#project-description)\
+[Repo Structure](#repo-structure)\
+[PostgreSQL Database Diagram](#postgresql-database-diagram)\
+[Sources](#sources)\
+[License](#license)\
+[Disclaimers](#disclaimers)
+
+
+## Quick Navigation
+
+* [Results folder (including SQL query scripts)](sql_queries_and_outputs/)
+* [PostgreSQL schema script](schema.sql)
+
+## Project Description
+
+This project explores the relationships between sporting strength and various economic and demographic metrics of countries and territories from across the world.
+
+The following economic and demographic metrics were analyzed in this project:
+* Economic metrics: GDP per capita
+* Demographic metrics: Population size
+
+The medal performances at recent Olympic Games (OG) were chosen to represent sporting strength as the OG is the most prestigious and recognizable sporting event that encompasses over 30 sports and over 200 delegations from across the world. 
+
+The raw data were processed in Python. Input datasets in CSV format were imported into a **PostgreSQL** database for query. 
+
+## Repo Structure
+
+* `database_csvs/`: input datasets
+    * `*.csv`: input datasets, including medal counts and demographic and economic datasets
+    * `input_data_descriptions.md`: detailed descriptions of the input datasets 
+* `schema.sql`: the SQL script for creating the PostgreSQL database schema used in this project
+* `sql_queries_and_outputs/`: SQL queries and their outputs
+    * `s*_*_and_medals.sql`: SQL query scripts
+        * `s1_exploratory_analyses.sq`: SQL queries for the exploratory analyses, such as the top performing delegations and continents at recent OGs
+        * `s2_econ_and_medals.sql`: SQL queries for GDP per capita-related analysis
+        * `s3_population_and_medals.sql`: SQL queries for population size-related analysis
+    * `s*_*_and_medals.md`: The SQL queries and their associated outputs presented in a markdown format. The contents of these files are same as the identically named `s*_*_and_medals.sql` files but are presented in a markdown format for ease of visualization and navigation.
+* `LICENSE.txt`: License agreement
+* `misc/`: Miscellaneous items, including disclaimer statements.
+
+
+## PostgreSQL Database Diagram
+
+![Schema](misc/assets/schema.png)
+
+## Sources
+
+For detailed descriptions of the origins of the input data and sources consulted, please see [input_data_descriptions.md](database_csvs/input_data_descriptions.md).
+
+## License
+
+This repository is distributed under a Creative Commons Attribution Share Alike 4.0 International (CC-BY-SA-4.0) [license](LICENSE.txt).
+
+## Disclaimers
+
+This repository represents a personal SQL data analytic project. The information presented here are for informational purpose only and are provided on an as-is basis without any warranties. This repo is not intended to represent the personal opinions of the author and should not be construed as such in any way. **Please carefully review the full [disclaimer statements](misc/disclaimers.md).**
+
+Thank you for checking out this project! I hope you enjoy these analyses, and best of luck to your favorite athletes on their quest for their next medal, whichever teams you support!
---
+++ schema.sql
@@ -0,0 +1,166 @@
+-- This is the script for creating the schema for this project
+-- the csv files in the database_csvs/ subdirectory will be imported into the tables created here
+-- For detailed descriptions of the input datasets, please consult the database_csvs/input_data_descriptions.md
+
+
+create schema if not exists og_demeco;
+
+-- noc info: names and codes of noc/npc and which geographical entity they correspond to 
+-- import the database_csvs/noc_to_demeco_entity_conversion_table.csv to this table
+create table if not exists noc_info (
+	noc_code CHAR(3) primary key,
+	noc_name VARCHAR(75),
+	demeco_entity_name VARCHAR(75)
+);
+
+-- demeco entity info: names of each geographical entity represented in the demographic and economic (demeco) datasets and which noc/npc they correspond to
+-- import demeco_entity_to_noc_conversion_table.csv here
+create table if not exists demeco_entity_info (
+	demeco_entity_name VARCHAR(75) primary key,
+	noc_code CHAR(3),
+	noc_name VARCHAR(75),
+	foreign key (noc_code) references noc_info(noc_code)
+);
+
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- medal count tables
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- medal tables for recent OGs
+-- import the database_csvs/medal_table_20*_o.csv files into the following tables. make sure the year matches
+
+create table if not exists medal_table_2024 (
+	noc_code CHAR(3) primary key,
+	gold INT,
+	silver INT,
+	bronze INT,
+	foreign key (noc_code) references noc_info(noc_code)
+);
+
+create table if not exists medal_table_2022 (
+	noc_code CHAR(3) primary key,
+	gold INT,
+	silver INT,
+	bronze INT,
+	foreign key (noc_code) references noc_info(noc_code)
+);
+
+
+create table if not exists medal_table_2020 (
+	noc_code CHAR(3) primary key,
+	gold INT,
+	silver INT,
+	bronze INT,
+	foreign key (noc_code) references noc_info(noc_code)
+);
+
+create table if not exists medal_table_2018 (
+	noc_code CHAR(3) primary key,
+	gold INT,
+	silver INT,
+	bronze INT,
+	foreign key (noc_code) references noc_info(noc_code)
+);
+
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- tables for the demographic and economic (demeco) datasets
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*
+create tables for the demographic/economical datasets (demeco datasets)
+here, entity_name are used as foreign key to reference the entity_name column of the demeco_entity_info table. 
+However, lots of geographical entities in these demeco datasets are not included in the demeco_entity_info table, 
+reason being that the demeco_entity_info table only includes entities that have a corresponding NOC that participate at the OGs.
+many of the entities in the demeco datasets do not have a corresponding noc that participate at the OGs (e.g. Scotland, French Polynesia, etc.), 
+To avoid SQL's foreign key error when importing data, data will be first loaded into a staging table that has the same structure as the actual table 
+but without the foreign key constraint. 
+Then we import the staging table into the actual table after fitlering out entities that are not part of the demeco_entity_info table.
+*/
+
+-- GPD per capita
+-- import the database_csvs/gdp_per_capita.csv into this table
+create table if not exists gdp_per_capita_staging_table (
+	entity_name VARCHAR(75),
+	year_of_data INT,
+	gdp_per_capita_val NUMERIC
+);
+
+
+create table if not exists gdp_per_capita (
+	entity_name VARCHAR(75),
+	year_of_data INT,
+	gdp_per_capita_val NUMERIC,
+	foreign key (entity_name) references demeco_entity_info(demeco_entity_name)
+);
+
+insert into gdp_per_capita (entity_name, year_of_data, gdp_per_capita_val)
+select * 
+from gdp_per_capita_staging_table
+where entity_name in (select demeco_entity_name from demeco_entity_info)
+on CONFLICT DO NOTHING; 
+
+-- populations
+-- import the database_csvs/populations.csv into this table
+create table if not exists pop_staging_table (
+	entity_name VARCHAR(75),
+	year_of_data INT,
+	populations FLOAT
+);
+
+
+create table if not exists pop (
+	entity_name VARCHAR(75),
+	year_of_data INT,
+	populations FLOAT,
+	foreign key (entity_name) references demeco_entity_info(demeco_entity_name)
+);
+
+insert into pop (entity_name, year_of_data, populations)
+select * 
+from pop_staging_table
+where entity_name in (select demeco_entity_name from demeco_entity_info)
+on CONFLICT DO NOTHING; 
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- tables for additional information regarding each noc/npc
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- geographical affiliations information of the NOCs (geo_info in short)
+-- use the following codes to update the noc_info table with additional information regarding each noc/npc
+-- currently only the continent info for each noc/npc are included in this repo
+-- may expand to include additional info such as subcontinental_regions in the future
+
+-- create a temp table
+create table if not exists geo_info (
+	noc_code CHAR(3) primary key,
+	noc_name VARCHAR(75),
+	continent VARCHAR(20),
+	subcontinental_regions VARCHAR(50),
+	hemisphere VARCHAR(6),
+	foreign key (noc_code) references noc_info(noc_code)
+);
+-- then import the database_csvs/noc_to_demeco_entity_conversion_table.csv to the table named geo_info
+-- just the noc_code and the continent column 
+-- for now only continent info is provided. additional geo info may be added in future updates
+
+
+-- then update the noc_info table
+alter table noc_info 
+add column continent VARCHAR(20);
+
+update noc_info 
+set continent = geo_info.continent
+from geo_info
+where noc_info.noc_code = geo_info.noc_code;
+
+
+-- check
+select *
+from noc_info
+limit 10;
+
+-- optional
+drop table if exists geo_info; 
+
